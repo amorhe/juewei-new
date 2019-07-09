b21a4fdca5a3666d06c22ac474bdb3a722a00fee
bevexed <547551650@qq.com>
2019-07-09 18:33:03 +0800

    地址数据绑定

--- package_vip/pages/waitpay/waitpay.js
+++ package_vip/pages/waitpay/waitpay.js
@@ -3,15 +3,45 @@ Page({
   data: {
     imageUrl,
 
-    sex:0
+
+  // 地址
+    name:'',
+
+    sex:0,
+
+    phone:'',
+
+    labelList:['学校','家','公司'],
+
+    curLabel:0
   },
   onLoad() {},
 
+
+
+
+// 地址
   changeSex(){
     const {sex} = this.data;
 
     this.setData({
       sex:sex === 0? 1: 0
     })
-  }
+  },
+
+changeCur(e){
+  let curLabel = e.currentTarget.dataset.cur
+  if(curLabel === this.data.curLabel) curLabel = '-1'
+  this.setData({curLabel})
+},
+
+handelChange(e){
+  console.log
+(e)
+  let {key} = e.currentTarget.dataset;
+  let {value} = e.detail;
+  this.setData({[key]:value})
+}
+
+
 });
