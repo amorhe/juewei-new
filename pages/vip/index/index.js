c5898fb416ad19f9d08850e607af53d72a5736a4
bevexed <547551650@qq.com>
2019-07-09 10:23:58 +0800

    vip 页面接口测试

--- pages/vip/index/index.js
+++ pages/vip/index/index.js
@@ -1,4 +1,18 @@
+import { imageUrl } from '../../common/js/baseUrl'
+import { ajax } from '../../common/js/li-ajax'
+
+const log = console.log
+
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    imageUrl
+  },
+  onLoad() {
+    this.getBanner()
+  },
+
+  async getBanner() {
+    let res = await ajax('/mini/vip/wap/banner/banner_list')
+    log(res)
+  }
 });
