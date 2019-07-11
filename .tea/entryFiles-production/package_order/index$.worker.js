a237e94eef3f36dd2fb8a787ae55c767e27cef7e
bevexed <547551650@qq.com>
2019-07-11 11:32:36 +0800

    删除环境配置文件

--- .tea/entryFiles-production/package_order/index$.worker.js
+++
@@ -1,5 +0,0 @@
-function success(){
-require('../../../package_order/pages/orderdetail/orderdetail');
-require('../../../package_order/pages/comment/comment');
-}
-self.bootstrapSubPackage('package_order', {success});
