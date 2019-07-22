b030866af688723e8bc6a702a6ca0be74593bb37
bevexed <547551650@qq.com>
2019-07-22 14:47:42 +0800

    积分页面数据接入

--- package_vip/pages/pointlist/pointlist.js
+++ package_vip/pages/pointlist/pointlist.js
@@ -1,13 +1,41 @@
 import { imageUrl } from '../../../pages/common/js/baseUrl'
+import { ajax, log } from '../../../pages/common/js/li-ajax'
 Page({
   data: {
     imageUrl,
+    userPoint:0,
+    list: []
   },
-  onLoad() {},
-  toUrl(e){
+  async onLoad() {
+    await this.getDetail()
+    await this.getUserPoint()
+  },
+
+  /**
+   * @function 获取积分详情
+   */
+  async getDetail() {
+    let res = await ajax('/mini/point_exchange/point_list', {}, 'GET')
+    if (res.code === 100) {
+      this.setData({
+        list: res.data.data
+      })
+    }
+  },
+  toUrl(e) {
     var url = e.currentTarget.dataset.url
     my.navigateTo({
-      url:url
+      url: url
     });
+  },
+
+  async getUserPoint(){
+    let {_sid} = this.data;
+    let res = await ajax('/mini/user/user_point',{_sid})
+    if(res.CODE === 'A100'){
+      this.setData({
+        userPoint:res.DATA
+      })
+    }
   },
 });
