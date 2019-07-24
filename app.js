10e476230d06cedfbae9e28e7329f34595ec03ea
userName <li12233@qq.com>
2019-07-24 17:49:39 +0800

    登录

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {loginByAliUid} from './pages/common/js/login'
+import {loginByAliUid,getuserInfo} from './pages/common/js/login'
 import {baseUrl} from './pages/common/js/baseUrl'
 App({
    onLaunch(options) {
@@ -17,11 +17,33 @@ App({
        })
       },
     });
+    this.getUserInfo()
   },
   onShow(options) {//多次执行
     // 从后台被 scheme 重新打开
     // console.log(options.query);
     // options.query == {number:1}
+  },
+  getUserInfo(){
+    var that = this
+    var _sid = my.getStorageSync({
+      key: '_sid', // 缓存数据的key
+    }).data;
+    getuserInfo(_sid||'').then(res=> {
+      console.log(res,'我的页面')
+      if(res.code==30106){
+        my.setStorageSync({
+          key: 'loginId', // 缓存数据的key
+          data: res.code, // 要缓存的数据
+        });
+      }
+      if(res.code==0){
+        my.setStorageSync({
+          key: 'loginId', // 缓存数据的key
+          data: res.code, // 要缓存的数据
+        });
+      }
+    })
   },
   onHide(){
     // 当小程序从前台进入后台时触发
--- package_my/pages/myaddress/selectaddress/selectaddress.js
+++ package_my/pages/myaddress/selectaddress/selectaddress.js
@@ -2,6 +2,59 @@ import { imageUrl } from '../../../../pages/common/js/baseUrl'
 Page({
   data: {
     imageUrl,
+    mapiconInfo:'',
+    // latitude:'30.305654',
+    // longitude:'120.093352'
+    list:[],
   },
-  onLoad() {},
+  onLoad() {
+    this.getLocation()
+  },
+  getLocation(){
+    var that =this
+    my.getLocation({
+      type:3,
+      success(res) {
+        my.hideLoading();
+        console.log(res)
+        var mapInfo = {
+          iconPath:imageUrl+'position.png',
+          latitude:res.latitude,
+          longitude:res.longitude,
+          width: 32,
+          height: 32
+        }
+        that.setData({
+          latitude:res.latitude,
+          longitude:res.longitude,
+          mapInfo:mapInfo,
+          list:res.pois
+        })
+      },
+      fail() {
+        my.hideLoading();
+        my.alert({ title: '定位失败' });
+      },
+    })
+  },
+  tapMap(e){
+    console.log(e)
+    var mapInfo = {
+      iconPath:imageUrl+'position.png',
+      latitude:e.latitude,
+      longitude:e.longitude,
+      width: 32,
+      height: 32
+    }
+    this.setData({
+      latitude:e.latitude,
+      longitude:e.longitude,
+      mapInfo:mapInfo,
+    })
+    my.chooseLocation({
+      success:(res)=>{
+        console.log(res)
+      }
+    });
+  }
 });
