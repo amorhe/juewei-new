a75eb3cfbd49da4de3c574bd7e68600dd1138121
bevexed <547551650@qq.com>
2019-07-30 18:04:39 +0800

    兑换页面 商品详情

--- package_vip/pages/detail/detail.js
+++ package_vip/pages/detail/detail.js
@@ -1,4 +1,4 @@
-import { ajax, parseData, log } from '../../../pages/common/js/li-ajax'
+import { ajax, parseData,getSid, log } from '../../../pages/common/js/li-ajax'
 import { imageUrl2 } from '../../../pages/common/js/baseUrl'
 
 Page({
@@ -8,44 +8,44 @@ Page({
     openPoint: false,
     content: '',
     detail: {
-      "id": "355",
-      "goods_name": "123",
-      "total_num": "1",
-      "valid_num": "1",
-      "cate_id": "25",
-      "intro": "<p>123<\/p>",
-      "goods_type": "2",
-      "goods_detail_type": "4",
-      "gift_id": "473",
-      "exchange_type": "1",
-      "point": "1",
-      "amount": 0,
-      "start_time": "2019-06-22 00:00:00",
-      "end_time": "2019-07-31 23:59:59",
-      "receive_type": "2",
-      "get_start_time": "0000-00-00 00:00:00",
-      "get_end_time": "0000-00-00 00:00:00",
-      "scope_type": "3",
-      "company_id": "0",
-      "city_id": "1",
-      "district_id": "0",
-      "express_type": "1",
-      "express_fee": "0",
-      "exchange_limit_type": "1",
-      "exchange_limit_num": "111",
-      "exchange_day_num": "0",
-      "exchange_intro": "<p>123123<\/p>",
-      "sort_no": "12345",
-      "status": "2",
-      "create_time": "2019-06-22 14:47:58",
-      "update_time": "2019-06-22 14:47:58",
-      "goods_pic": [{
-        "id": "318",
-        "goods_pic": "\/static\/check\/image\/goods_point\/oXQW34ZBT6Pcbkx0.jpg"
-      }, {
-        "id": "454",
-        "goods_pic": "\/static\/check\/image\/goods_point\/noTHmy1mTM09NrRh.png"
-      }]
+    //   "id": "355",
+    //   "goods_name": "123",
+    //   "total_num": "1",
+    //   "valid_num": "1",
+    //   "cate_id": "25",
+    //   "intro": "<p>123<\/p>",
+    //   "goods_type": "2",
+    //   "goods_detail_type": "4",
+    //   "gift_id": "473",
+    //   "exchange_type": "1",
+    //   "point": "1",
+    //   "amount": 0,
+    //   "start_time": "2019-06-22 00:00:00",
+    //   "end_time": "2019-07-31 23:59:59",
+    //   "receive_type": "2",
+    //   "get_start_time": "0000-00-00 00:00:00",
+    //   "get_end_time": "0000-00-00 00:00:00",
+    //   "scope_type": "3",
+    //   "company_id": "0",
+    //   "city_id": "1",
+    //   "district_id": "0",
+    //   "express_type": "1",
+    //   "express_fee": "0",
+    //   "exchange_limit_type": "1",
+    //   "exchange_limit_num": "111",
+    //   "exchange_day_num": "0",
+    //   "exchange_intro": "<p>123123<\/p>",
+    //   "sort_no": "12345",
+    //   "status": "2",
+    //   "create_time": "2019-06-22 14:47:58",
+    //   "update_time": "2019-06-22 14:47:58",
+    //   "goods_pic": [{
+    //     "id": "318",
+    //     "goods_pic": "\/static\/check\/image\/goods_point\/oXQW34ZBT6Pcbkx0.jpg"
+    //   }, {
+    //     "id": "454",
+    //     "goods_pic": "\/static\/check\/image\/goods_point\/noTHmy1mTM09NrRh.png"
+    //   }]
     }
   },
 
@@ -197,10 +197,17 @@ Page({
   },
 
   /**
-   * @function 显示modal
+   * @function 显示modal，立即兑换
    */
 
   async showConfirm() {
+    let _sid = await getSid()
+    if(!_sid){
+      return my.showToast({
+       content:'用户未登录'
+      });
+    }
+
     let { goods_name, point } = this.data.detail
 
     // 获取 用户 积分
