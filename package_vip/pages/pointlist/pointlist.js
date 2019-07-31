f773ec4251333002a1efc876fd405b25835bab9a
bevexed <547551650@qq.com>
2019-07-31 11:36:19 +0800

    积分 明细 分页

--- package_vip/pages/pointlist/pointlist.js
+++ package_vip/pages/pointlist/pointlist.js
@@ -5,21 +5,35 @@ Page({
     imageUrl,
     userPoint:0,
     list: [],
-    toast:false
+    toast:false,
+    pagenum:1,
+    pagesize:10
   },
   async onLoad() {
-    await this.getDetail()
+    await this.getDetail(1)
     await this.getUserPoint()
+  },
+
+  async onReachBottom() {
+    // 页面被拉到底部
+    let { pagenum } = this.data
+    ++pagenum
+    await this.getDetail(pagenum)
+    this.setData({
+      pagenum
+    })
   },
 
   /**
    * @function 获取积分详情
    */
-  async getDetail() {
-    let res = await ajax('/mini/point_exchange/point_list', {}, 'GET')
+  async getDetail(pagenum) {
+    let {list,pagesize} = this.data
+    let res = await ajax('/mini/point_exchange/point_list', {pagenum,pagesize}, 'GET')
     if (res.code === 100) {
+      if(res.data.data.length == 0){return}
       this.setData({
-        list: res.data.data
+        list: [...list,...res.data.data]
       })
     }
   },
