1103aafdf1f548b899535043f51cba78af76e13a
userName <li12233@qq.com>
2019-08-02 14:33:49 +0800

    修改图片路径

--- package_my/pages/entitlement/entitlement.js
+++ package_my/pages/entitlement/entitlement.js
@@ -1,18 +1,20 @@
+import {imageUrl} from '../../../pages/common/js/baseUrl'
 Page({
   data: {
+    imageUrl,
     list:[
       {
-        icon:'../../../pages/common/img/entIcon.png',
+        icon:imageUrl+'my_quity_01.png',
         title:'迎新礼包',
         info:'VIP迎新礼包，价值100元优惠券',
       },
       {
-        icon:'../../../pages/common/img/entIconjue.png',
+        icon:imageUrl+'my_quity_02.png',
         title:'积分礼',
         info:'积分可在VIP专享兑换专属礼品',
       },
       {
-        icon:'../../../pages/common/img/entIconVIP.png',
+        icon:imageUrl+'my_quity_04.png',
         title:'VIP特惠日',
         info:'VIP特惠日，双倍积分、积分兑券、摇奖、商品特价等丰富的会 员活动',
       }
