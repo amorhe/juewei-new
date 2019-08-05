36751686b52beb347e936a5538fdc6315c145cd9
bevexed <547551650@qq.com>
2019-08-05 18:38:26 +0800

    优惠券 二维码 展示

--- package_my/pages/coupon/coupon.js
+++ package_my/pages/coupon/coupon.js
@@ -1,8 +1,11 @@
-import {imageUrl,imageUrl2} from '../../../pages/common/js/baseUrl'
-import {couponsList,exchangeCode} from '../../../pages/common/js/home'
-import {formatTime} from '../../../pages/common/js/time'
+import { imageUrl, imageUrl2, baseUrl } from '../../../pages/common/js/baseUrl'
+import { couponsList, exchangeCode } from '../../../pages/common/js/home'
+import { formatTime } from '../../../pages/common/js/time'
+import { getSid, log ,ajax} from '../../../pages/common/js/li-ajax'
 Page({
   data: {
+    open2: false,
+    codeImg: '',
     tabs: [
       {
         title: '优惠券0张'
@@ -14,35 +17,41 @@ Page({
     activeTab: 0,  // 初始选中
     imageUrl,
     imageUrl2,
-    couponList:[],  // 优惠券列表
-    exchangeList:[],  // 兑换列表
+    couponList: [],  // 优惠券列表
+    exchangeList: [],  // 兑换列表
   },
   onLoad() {
-    const _sid = my.getStorageSync({key: '_sid'}).data;
+    const _sid = my.getStorageSync({ key: '_sid' }).data;
     this.getCouponsList(_sid);
     this.getExchangeCode(_sid);
+    let phone = my.getStorageSync({ key: 'phone' }).data
+    this.setData({ phone })
   },
+  onHide() {
+    this.closeModel()
+  },
+
   // 优惠券
-  getCouponsList(_sid){
-    couponsList(_sid,'use').then((res) => {
+  getCouponsList(_sid) {
+    couponsList(_sid, 'use').then((res) => {
       this.data.tabs[0].title = `优惠券${res.DATA.use.length}张`;
       res.DATA.use.forEach(item => {
-        item.start_time = formatTime(item.start_time,'Y-M-D');
-        item.end_time = formatTime(item.end_time,'Y-M-D');
+        item.start_time = formatTime(item.start_time, 'Y-M-D');
+        item.end_time = formatTime(item.end_time, 'Y-M-D');
       })
       this.setData({
-        couponList:res.DATA.use,
-        tabs:this.data.tabs
+        couponList: res.DATA.use,
+        tabs: this.data.tabs
       })
     })
   },
   // 兑换码
-  getExchangeCode(_sid){
-    exchangeCode(_sid,'use').then((res) => {
+  getExchangeCode(_sid) {
+    exchangeCode(_sid, 'use').then((res) => {
       console.log(res)
-       this.data.tabs[1].title = `兑换码${res.DATA.length}个`;
+      this.data.tabs[1].title = `兑换码${res.DATA.length}个`;
       this.setData({
-        exchangeList:res.DATA
+        exchangeList: res.DATA
       })
     })
   },
@@ -52,9 +61,46 @@ Page({
     });
   },
   // 兑换详情
-  changedetail(){
+  changedetail() {
     my.navigateTo({
-      url:'/package_my/pages/coupon/changedetails/changedetails'
+      url: '/package_my/pages/coupon/changedetails/changedetails'
     })
+  },
+
+  /**
+   * @function 展示CODE
+   */
+
+  async showCode(e) {
+    let { code } = e.currentTarget.dataset
+    let _sid = await getSid()
+    let codeImg = baseUrl + '/juewei-api/coupon/getQRcode?' + '_sid=' + _sid + '&code=' + code
+    this.setData({
+      open2: true,
+      codeImg
+    })
+  },
+  /**
+   * @function 关闭弹窗
+   */
+  closeModel() {
+    this.setData({
+      open2: false
+    })
+  },
+
+   /**
+   * @function 核销
+   */
+
+  async wait() {
+    let res = await ajax('/juewei-api/order/waiting', {}, 'GET')
+    if (res.code == 0) {
+      return this.closeModel()
+    }
+
+    return my.showToast({
+      content: res.msg,
+    });
   }
 });
