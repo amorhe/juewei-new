272a6c03164a997d8fcc004f8da0cdf7feea3d12
UDO <udo@appledeMacBook-Air.local>
2019-08-14 21:09:30 +0800

    优惠券

--- pages/home/orderform/chooseCoupon/chooseCoupon.js
+++ pages/home/orderform/chooseCoupon/chooseCoupon.js
@@ -7,81 +7,87 @@ Page({
   data: {
     open2: false,
     codeImg: '',
-    active:[],
-    activeIndex:'',
+    active: [],
+    activeIndex: '',
     imageUrl,
     imageUrl2,
-    couponList:[],  // 优惠券列表
-    defaultcoupon:'',    // 默认选中的优惠券
-    couponChoosed:{}
+    couponList: [],  // 优惠券列表
+    defaultcoupon: '',    // 默认选中的优惠券
+    couponChoosed: {}
   },
   onLoad(e) {
     const _sid = my.getStorageSync({ key: '_sid' }).data;
-    this.getCouponsList(_sid,e.money/100);
+    this.getCouponsList(_sid, e.money / 100);
   },
   onHide() {
     this.closeModel();
   },
 
   // 优惠券
-  getCouponsList(_sid,money) {
+  getCouponsList(_sid, money) {
     this.data.couponChoosed = {};
-    couponsList(_sid, 'use',money,my.getStorageSync({key:'shop_id'}).data).then((res) => {
+    couponsList(_sid, 'use', money, my.getStorageSync({ key: 'shop_id' }).data).then((res) => {
       console.log(res)
       res.DATA.use.forEach(item => {
         item.start_time = formatTime(item.start_time, 'Y-M-D');
         item.end_time = formatTime(item.end_time, 'Y-M-D');
         item.toggleRule = false,
-        item.isChecked = false
+          item.isChecked = false
       })
       // 已选中的优惠券
-      if(app.globalData.coupon_code){
-        if(app.globalData.notUse==0){
+      if (app.globalData.coupon_code) {
+        if (app.globalData.notUse == 0) {
           this.data.couponChoosed[`e${res.DATA.use.findIndex(item => item.code == app.globalData.coupon_code)}`] = app.globalData.coupon_code;
         }
         this.setData({
           couponList: res.DATA.use,
-          couponChoosed:this.data.couponChoosed
+          couponChoosed: this.data.couponChoosed
         })
-      }else{
+      } else {
         // 默认选择的优惠券
         this.data.couponChoosed[`e${res.DATA.use.findIndex(item => item.code == res.DATA.max.code)}`] = res.DATA.max.code;
         this.setData({
           couponList: res.DATA.use,
-          couponChoosed:this.data.couponChoosed
+          couponChoosed: this.data.couponChoosed
         })
       }
 
     })
   },
-  chooseCouponed(e){
-    app.globalData.coupon_code = e.currentTarget.dataset.coupon_code;
-    if(Object.keys(this.data.couponChoosed).length==0){
+  chooseCouponed(e) {
+    let couponChoosed = {};
+    couponChoosed[`e${e.currentTarget.dataset.index}`] = e.currentTarget.dataset.coupon_code;
+    if (this.data.couponChoosed[`e${e.currentTarget.dataset.index}`] == e.currentTarget.dataset.coupon_code) {
       app.globalData.notUse = 1;
-    }else{
-      app.globalData.notUse = 0
+      this.setData({
+        couponChoosed: {}
+      })
+    } else {
+      app.globalData.notUse = 0;
+      this.setData({
+        couponChoosed
+      })
     }
+    app.globalData.coupon_code = e.currentTarget.dataset.coupon_code;
     my.navigateBack({
       url: '/pages/home/orderform/orderform'
     });
   },
   // 切换是否选中
-  selectTapTrue(e){
-    let couponChoosed = {};
-    couponChoosed[`e${e.currentTarget.dataset.index}`] = e.currentTarget.dataset.code;
-    app.globalData.notUse = 0
-    this.setData({
-      couponChoosed
-    })
-    my.navigateBack({
-      url: '/pages/home/orderform/orderform'
-    });
-  },
-  selectTapFalse(e){
-    this.setData({
-      couponChoosed:{}
-    })
-  },
+  // selectTapTrue(e) {
+  //   let couponChoosed = {};
+  //   couponChoosed[`e${e.currentTarget.dataset.index}`] = e.currentTarget.dataset.code;
+  //   app.globalData.notUse = 0;
+  //   this.setData({
+  //     couponChoosed
+  //   })
+  // },
+  // selectTapFalse(e) {
+  //   app.globalData.notUse = 1;
+  //   this.setData({
+  //     couponChoosed: {}
+  //   })
+  // },
 
   /**
    * @function 展示CODE
