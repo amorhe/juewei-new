696218064213a968fb963f3dc9dd932fdbc71c9c
fuyu <449066499@qq.com>
2019-08-21 15:59:16 +0800

    修正配置文件

--- pages/common/js/baseUrl.js
+++ pages/common/js/baseUrl.js
@@ -1,32 +1,35 @@
-// 测试主域名
-// export const baseUrl = 'https://dev.juewei.com/api';
+// //测试环境
 // export const baseUrl = 'https://test-wap.juewei.com/api';
+// // 图片测试cdn
+// export const imageUrl = 'https://test-wap.juewei.com/m/ali-mini/image/';
+// export const imageUrl2 = 'https://imgcdnjwd.juewei.com';
+// export const imageUrl3 = 'https://images.juewei.com';
+// //jsonUrl
+// export const jsonUrl='https://imgcdnjwd.juewei.com/static/check';
+// //百度测试ak
+// export const ak = 'pRtqXqnajTytAzWDL3HOnPRK';
+// export const geotable_id='134917';
 
-// 生产域名
+
+
+
+// 生产环境
 export const baseUrl = 'https://wap.juewei.com/api';
-
-// 判断是否测试环境
-const isTestUrl = baseUrl.includes('test');
 // 图片测试cdn
 export const imageUrl = 'https://wap.juewei.com/m/ali-mini/image/';
 export const imageUrl2 = 'https://imgcdnjwd.juewei.com';
 export const imageUrl3 = 'https://images.juewei.com';
-
 //jsonUrl
-// 测试
-// export const jsonUrl='https://imgcdnjwd.juewei.com/static/check';
-// 正式
 export const jsonUrl='https://imgcdnjwd.juewei.com/static/product';
-
 // 百度生产ak
 export const ak = 'pRtqXqnajTytAzWDL3HOnPRK';
 export const geotable_id='134917';
-//百度测试ak
-// export const ak = 'kLRuRCHmGvOYIqxmwEzI9PpC80lfuSCU';
-// export const geotable_id='170580';
- 
+
+
+
+
+// 判断是否测试环境
+const isTestUrl = baseUrl.includes('test');
 // 套餐图片路径
 export const img_url = isTestUrl?imageUrl2:imageUrl3;
-// const imageUrl = 'https://wap.juewei.com/m/ali-mini/image/';
-
 
