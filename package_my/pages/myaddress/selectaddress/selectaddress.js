31e2095e23cc6c6199150947b2c7f3a761b960bb
JWD-PC\JWD <fuyu@juewei.com>
2019-11-27 19:59:02 +0800

    修正选地址页面

--- package_my/pages/myaddress/selectaddress/selectaddress.js
+++ package_my/pages/myaddress/selectaddress/selectaddress.js
@@ -17,6 +17,7 @@ Page({
     inputAddress: '',  //手动输入的地址
     loginOpened: false,
     addressList: [],
+    addressList_poi:[],//poi数据
     currentPos: '',
     noSearchResult: false,
     mapCtx: {},
@@ -51,12 +52,12 @@ Page({
       })
     }, 1000)
   },
+  //获取当前定位地址信息
   getLocation() {
     var that = this
     my.getLocation({
       type: 3,
       success(res) {
-        console.log(res)
         my.hideLoading();
         let currentPos = res.pois[0] ? res.pois[0].name : currentPos;
         that.setData({
@@ -64,9 +65,43 @@ Page({
           latitude: res.latitude,
           city: res.city,
           currentPos: currentPos,
-          addressList: res.pois
+          addressList: [],//这里地址不能是支付宝的高德地图的
+          addressList_poi:[]
         });
-        that.setIcon(res.longitude, res.latitude)
+        //获取附近的绝味鸭脖门店显示
+        that.setIcon(res.longitude, res.latitude);
+        let baiduPos = bd_encrypt(res.longitude, res.latitude);// 百度经纬度
+        //获取附近的poi信息
+        let url = `https://api.map.baidu.com/reverse_geocoding/v3/?ak=${ak}&output=json&coordtype=bd09ll&location=${baiduPos.bd_lat},${baiduPos.bd_lng}&extensions_poi=1`;
+        url = encodeURI(url);
+        my.request({
+          url,
+          success: (res) => {
+            if (res.data.status === 0) {
+              let result = res.data.result.pois;
+              let addressComponent = res.data.result.addressComponent
+              // 无数据显示无数据页面
+              if (result.length === 0) {
+                that.setData({
+                  noSearchResult: true
+                })
+              }
+              result.forEach(item => {
+                item.province = addressComponent.province,
+                  item.city = addressComponent.city,
+                  item.district = addressComponent.district
+                item.location = {
+                  lng: item.point.x,
+                  lat: item.point.y
+                }
+              })
+              that.setData({
+                addressList_poi: result,
+                noSearchResult: false
+              })
+            }
+          },
+        });
       },
       fail() {
         my.hideLoading();
@@ -98,6 +133,7 @@ Page({
     //   isSearch: true
     // })
   },
+  //通过经纬度设置地图上的点位置附近2000米绝味鸭脖的门店
   setIcon(long, lat) {
     // 从地图中获取的和往地图中设置的都是高德经纬度，接口调用和后台存储的都是百度的经纬度，在使用中要注意转换
     var that = this
@@ -106,7 +142,6 @@ Page({
       url: 'https://api.map.baidu.com/geosearch/v3/nearby?ak=' + ak + '&geotable_id=' + geotable_id + '&location=' + baiduPos.bd_lng + ',' + baiduPos.bd_lat + '&radius=2000',
       success: (res) => {
         // 设置定位位置的图标
-
         if (!this.data.sysInfo) {
           let sysInfo = my.getSystemInfoSync();
           this.setData({
@@ -135,17 +170,18 @@ Page({
           obj.height = 20
           arr.push(obj)
         })
-
         that.setData({
           mapInfo: arr
         })
       },
     });
   },
-  // 输入
+
+  // 输入事件
   searchInput(e) {
     if (e.detail.value == '') {
       this.setData({
+        inputAddress:'',
         noSearchResult: false,
         isSearch: false
       })
@@ -154,6 +190,8 @@ Page({
     this.setData({
       inputAddress: e.detail.value
     })
+    //搜索
+    this.searchAddress();
   },
   // 搜索
   searchAddress() {
@@ -179,17 +217,23 @@ Page({
           })
         }
         // 搜索到的位置
-        let centerPos = res.data.results[0];
-        if (centerPos) {
-          const gd_pos = gd_decrypt(centerPos.location.lng, centerPos.location.lat);
-          this.setData({
-            longitude: gd_pos.lng,
-            latitude: gd_pos.lat
-          });
-          this.setIcon(centerPos.location.lng, centerPos.location.lat);
-        }
-
+        // let centerPos = res.data.results[0];
+        // if (centerPos) {
+        //   const gd_pos = gd_decrypt(centerPos.location.lng, centerPos.location.lat);
+        //   this.setData({
+        //     longitude: gd_pos.lng,
+        //     latitude: gd_pos.lat
+        //   });
+        //   this.setIcon(centerPos.location.lng, centerPos.location.lat);
+        // }
       },
+      fail: (e) => {
+          this.setData({
+            addressList: [],
+            noSearchResult: false,
+            isSearch: true
+          })
+      }
     });
   },
   chooseAdress(e) {
@@ -197,21 +241,22 @@ Page({
     app.globalData.addAddressInfo = pos;
     my.navigateBack();
   },
+  //选择地图上的点，或者拖动地图后
   mapTap(e) {
-    if (!this.data.movePosFlag) {
-      return;
-    }
+    let that = this;
+    if (!this.data.movePosFlag) { return; }
     if (e.type == 'end' || e.type == 'tap') {
-
       let newLat = e.latitude;
       let newLng = e.longitude;
-      let that = this;
+      //将周边的门店显示到地图上
       that.setIcon(e.longitude, e.latitude);
+      //转换成百度的
       let baiduPos = bd_encrypt(e.longitude, e.latitude);
       this.mapCtx.updateComponents({
         longitude: e.longitude,
         latitude: e.latitude,
-      })
+      });
+      //获取附近的poi信息
       let url = `https://api.map.baidu.com/reverse_geocoding/v3/?ak=${ak}&output=json&coordtype=bd09ll&location=${baiduPos.bd_lat},${baiduPos.bd_lng}&extensions_poi=1`;
       url = encodeURI(url);
       my.request({
@@ -226,7 +271,6 @@ Page({
                 noSearchResult: true
               })
             }
-
             result.forEach(item => {
               item.province = addressComponent.province,
                 item.city = addressComponent.city,
@@ -237,13 +281,12 @@ Page({
               }
             })
             this.setData({
-              addressList: result,
+              addressList_poi: result,
               noSearchResult: false
             })
           }
         },
       });
-
     }
-  }
+  },
 });
